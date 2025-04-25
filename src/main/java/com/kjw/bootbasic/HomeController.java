7f93ccabaf95063babeb00f3c4a06684437530b6
goottjason <goottjason@gmail.com>
2025-04-25 16:53:51 +0900

    250425 end

--- src/main/java/com/kjw/bootbasic/HomeController.java
+++ src/main/java/com/kjw/bootbasic/HomeController.java
@@ -6,65 +6,139 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 public class HomeController {
 
-    // templates의 index.html을 찾아줌
-    @GetMapping("/")
-    public String home() {
-        return "index";
-    }
+  // templates의 index.html을 찾아줌
+  @GetMapping("/")
+  public String home() {
+    return "index";
+  }
 
-    @GetMapping("/textbasic")
-    public String textBasic(Model model) {
-        model.addAttribute("data", "Hello, Thymeleaf!");
-        model.addAttribute("unescapedData", "<strong>Hello, Thymeleaf!</strong>");
-        return "/basic/textbasic";
-    }
+  @GetMapping("/textbasic")
+  public String textBasic(Model model) {
+    model.addAttribute("data", "Hello, Thymeleaf!");
+    model.addAttribute("unescapedData", "<strong>Hello, Thymeleaf!</strong>");
+    return "/basic/textbasic";
+  }
 
-    @GetMapping("/variable")
-    public String variable(Model model) {
-        User hong = new User("hong", 10);
-        User choi = new User("choi", 20);
+  @GetMapping("/variable")
+  public String variable(Model model) {
+    User hong = new User("hong", 10);
+    User choi = new User("choi", 20);
 
-        List<User> list = new ArrayList<>();
-        list.add(hong);
-        list.add(choi);
+    List<User> list = new ArrayList<>();
+    list.add(hong);
+    list.add(choi);
 
-        Map<String, User> map = new HashMap<>();
-        map.put("choi", choi);
-        map.put("hong", hong);
+    Map<String, User> map = new HashMap<>();
+    map.put("choi", choi);
+    map.put("hong", hong);
 
-        model.addAttribute("user", choi);
-        model.addAttribute("users", list);
-        model.addAttribute("userMap", map);
+    model.addAttribute("user", choi);
+    model.addAttribute("users", list);
+    model.addAttribute("userMap", map);
 
-        return "/basic/variable";
-    }
+    return "/basic/variable";
+  }
 
-    @GetMapping("/basicobject")
-    public String basicObject(Model model, HttpSession session) {
-        model.addAttribute("modelData", "모델데이터");
-        session.setAttribute("sessionData", "세션데이터");
-        return "/basic/basicobject";
-    }
+  @GetMapping("/basicobject")
+  public String basicObject(Model model, HttpSession session) {
+    model.addAttribute("modelData", "모델데이터");
+    session.setAttribute("sessionData", "세션데이터");
+    return "/basic/basicobject";
+  }
 
-    @GetMapping("/link")
-    public String link(Model model) {
-        model.addAttribute("name", "hong");
-        model.addAttribute("age", "10");
-        return "/basic/link";
-    }
+  @GetMapping("/link")
+  public String link(Model model) {
+    model.addAttribute("name", "hong");
+    model.addAttribute("age", "10");
+    return "/basic/link";
+  }
 
-    @GetMapping("/date")
-    public String date(Model model) {
-        model.addAttribute("localDateTime", LocalDateTime.now());
-        return "/basic/date";
-    }
+  @GetMapping("/date")
+  public String date(Model model) {
+    model.addAttribute("localDateTime", LocalDateTime.now());
+    model.addAttribute("calendar", Calendar.getInstance());
+    model.addAttribute("date", new Date());
+    model.addAttribute("timestamp", new Timestamp(System.currentTimeMillis()));
+
+    return "/basic/date";
+  }
+
+  @GetMapping("/literal")
+  public String literal(Model model) {
+    model.addAttribute("data", "thymeleaf");
+    model.addAttribute("isValid", true);
+    model.addAttribute("isNull", null);
+    return "/basic/literal";
+  }
+
+  @GetMapping("/operation")
+  public String operation(Model model) {
+    model.addAttribute("isValid", true);
+    model.addAttribute("isNull", null);
+    model.addAttribute("data", "thymeleaf");
+
+    return "/basic/operation";
+  }
+  @GetMapping("/attr")
+  public String attr() {
+
+    return "/basic/attr";
+  }
+
+  @GetMapping("/theach")
+  public String theach(Model model) {
+
+    User hong = new User("hong", 10);
+    User choi = new User("choi", 20);
+    User lee = new User("lee", 21);
+
+    List<User> list = new ArrayList<>();
+    list.add(hong);
+    list.add(choi);
+    list.add(lee);
+
+    Map<String, User> map = new HashMap<>();
+    map.put("choi", choi);
+    map.put("hong", hong);
+    map.put("lee", lee);
+
+    model.addAttribute("users", list);
+    model.addAttribute("map", map);
+    return "/basic/theach";
+  }
+  @GetMapping("/comments")
+  public String comments() {
+
+    return "/basic/comments";
+  }
+  @GetMapping("/inlinejs")
+  public String inlinejs(Model model) {
+    model.addAttribute("user", new User("hong", 10));
+
+    User hong = new User("hong", 10);
+    User choi = new User("choi", 20);
+    User lee = new User("lee", 21);
+
+    List<User> list = new ArrayList<>();
+    list.add(hong);
+    list.add(choi);
+    list.add(lee);
+
+    Map<String, User> map = new HashMap<>();
+    map.put("choi", choi);
+    map.put("hong", hong);
+    map.put("lee", lee);
+
+    model.addAttribute("users", list);
+    model.addAttribute("map", map);
+
+  return "/basic/inlinejs";
+  }
 }
